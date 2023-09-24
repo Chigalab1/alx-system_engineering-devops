23300deb97309df7e4ff78c32f9d20a5e310d5ca
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-09-24 11:51:08 -0700

    confi

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# Change the SSH Client confuration file
+# Change the SSH Client config file
 file { 'etc/ssh/ssh_config':
 	ensure => present,
 content =>"
 
-	# ssh host client config
+	# ssh host client configuration
 	host*
   	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
