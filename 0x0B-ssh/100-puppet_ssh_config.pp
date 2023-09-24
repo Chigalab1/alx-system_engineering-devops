6a19b36e36bc54f47a2211f27cdca30349bba525
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-09-24 11:54:27 -0700

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
-# Change the SSH Client config file
-file { 'etc/ssh/ssh_config':
-	ensure => present,
-content =>"
-
-	# ssh host client configuration
-	host*
-  	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
-}
+# set up your client SSH configuration file so that you can connect to a server without typing a password.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  }
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+  }
