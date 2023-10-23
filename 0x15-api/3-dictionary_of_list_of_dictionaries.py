9cac6cce5875723cf432e31d7d10b96ad3a52c98
root <Chigozirim Uwaezuoke>
2023-10-23 11:35:32 -0700

    executable

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 Script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress
-and export data in the JSON format.
+information about his ir her ToDo list progress
 """
 
 import json
@@ -22,15 +21,15 @@ if __name__ == "__main__":
     todos = todos.json()
     todoAll = {}
 
-    for user in users:
+    for u in users:
         taskList = []
         for task in todos:
-            if task.get('userId') == user.get('id'):
-                taskDict = {"username": user.get('username'),
+            if task.get('userId') == u.get('id'):
+                taskDict = {"username": u.get('username'),
                             "task": task.get('title'),
                             "completed": task.get('completed')}
                 taskList.append(taskDict)
-        todoAll[user.get('id')] = taskList
+        todoAll[u.get('id')] = taskList
 
     with open('todo_all_employees.json', mode='w') as f:
         json.dump(todoAll, f)
