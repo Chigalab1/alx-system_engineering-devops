9ae43413eb8481700f6a88359d5ee65d001cc943
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-11-16 11:28:06 +0000

    uptdate

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 #increase hard file limit for Holberton User.
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed-i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin/'
 }
 
 # increase soft file limit for holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
